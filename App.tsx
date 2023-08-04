5531d5a33fd6c44a8a1a39226a3683479e692f25
AnithaGummalla <109032010+AnithaGummalla@users.noreply.github.com>
2023-08-04 14:57:20 +0530

    modified App.tsx

--- App.tsx
+++ App.tsx
@@ -1,15 +1,10 @@
 import {View, Text} from 'react-native';
 import React from 'react';
-import TailwindTest from './src/TailwindTest';
 import FlatListCalculator from './src/FlatListCalculator';
-import {CalculatorInput} from 'react-native-calculator';
-// import { Calculator } from 'react-native-calculator';
 
 const App = () => {
   return (
     <View style={{flex: 1}}>
-      {/* <TailwindTest /> */}
-      {/* <Calculator /> */}
       <FlatListCalculator />
     </View>
   );
